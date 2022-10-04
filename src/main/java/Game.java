9a3226245c33a81c3351db9fb9988535a7ef4168
MitsukiS16 <clarissemtcarvalho@gmail.com>
2022-10-04 16:31:05 +0100

    [A] Reading keystrokes

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,32 +1,17 @@
 import com.googlecode.lanterna.TerminalSize;
 import com.googlecode.lanterna.TextCharacter;
+import com.googlecode.lanterna.input.KeyStroke;
 import com.googlecode.lanterna.screen.Screen;
 import com.googlecode.lanterna.screen.TerminalScreen;
 import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
 import com.googlecode.lanterna.terminal.Terminal;
 
-import javax.swing.*;
 import java.io.IOException;
 
 public class Game {
     private int x = 10;
     private int y = 10;
     Screen screen;
-
-    private void draw() throws IOException {
-        screen.clear();
-        screen.setCharacter(10, 10, TextCharacter.fromCharacter('X')[0]);
-        screen.refresh();
-    }
-
-    public void run() throws IOException {
-        draw();
-    }
-
-    private void processKey(KeyStroke key) {
-        System.out.println(key);
-    }
-
 
     Game() {
         try {
@@ -38,9 +23,39 @@ public class Game {
             screen.setCursorPosition(null); // we don't need a cursor
             screen.startScreen(); // screens must be started
             screen.doResizeIfNecessary(); // resize screen if necessary
+            KeyStroke key = screen.readInput();
 
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
+
+    private void draw() throws IOException {
+        screen.clear();
+        screen.setCharacter(x, y, TextCharacter.fromCharacter('X')[0]);
+        screen.refresh();
+    }
+
+    public void run() throws IOException {
+        draw();
+    }
+
+    private void processKey(KeyStroke key) {
+        System.out.println(key);
+
+
+        if (key.getKeyType() == KeyType.ArrowUp) {
+
+        }
+
+        if (key.getKeyType() == KeyType.Character && key.getCharacter() == 'q') {
+
+        }
+
+
+    }
+
+
+
+
 }
