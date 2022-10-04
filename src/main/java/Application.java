ec044d9e1501a911dcada6cd6b195978663f1dd0
MitsukiS16 <clarissemtcarvalho@gmail.com>
2022-10-04 12:07:55 +0100

    [A] create new game

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,5 +1,24 @@
+import com.googlecode.lanterna.TerminalSize;
+import com.googlecode.lanterna.TextCharacter;
+import com.googlecode.lanterna.screen.Screen;
+import com.googlecode.lanterna.screen.TerminalScreen;
+import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
+import com.googlecode.lanterna.terminal.Terminal;
+
+import java.io.IOException;
+
 public class Application {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    static void main() throws IOException {
+        Game game = new Game();
+        game.run();
+/*
+        private void draw() throws IOException {
+            screen.clear();
+            hero.draw(screen);
+            screen.refresh();
+        }
+*/
     }
+
+
 }
--- src/main/java/com/MitsukiS16/hero/Main.java
+++
@@ -1,7 +0,0 @@
-package com.MitsukiS16.hero;
-
-public class Main {
-    public static void main(String[] args) {
-        //System.out.println("Hello world!");
-    }
-}
